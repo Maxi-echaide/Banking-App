9e6373e0bfa29f3b600d89fd69d0cf1d030d54ad
Maxi-echaide <maxi.echaide@gmail.com>
2024-07-03 05:21:54 -0300

    charts, animatedCounter, sidebar, hamburger

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,13 +1,18 @@
-
+import MobileNav from '@/components/MobileNav';
+import Sidebar from '@/components/Sidebar';
+import Image from 'next/image';
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  
+
   return (
     <main>
-      {children}
+      
+        {children}
     </main>
   );
 }
