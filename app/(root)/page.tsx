c34824be37b1af92a284ad763c69b7b429faccaf
Maxi-echaide <maxi.echaide@gmail.com>
2024-07-03 07:57:53 -0300

    RightSidebar, bankCard, connect routing

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,10 @@
 import HeaderBox from '@/components/HeaderBox'
+import RightSideBar from '@/components/RightSideBar';
 import TotalBalanceBox from '@/components/TotalBalanceBox';
 import React from 'react'
 
 const Home = () => {
-  const loggedIn = {firstName: "Max"}
+  const loggedIn = {firstName: "Max", lastName: "echa", email:"maxi.echaide@gmail.com"}
   return (
     <section className='home'>
       <div className='home-content'>
@@ -11,7 +12,11 @@ const Home = () => {
           <HeaderBox type='greeting' title='Welcome' user={loggedIn?.firstName || "Guest"} subtext="Acces and manage your account and transactions efficiently"/>
           <TotalBalanceBox accounts={[]} totalBanks={1} totalCurrentBalance={1250.35} />
         </header>
+
+        RECENT TRABSACUTUIB
       </div>
+
+      <RightSideBar user={loggedIn} transactions={[]} banks={[{},{}]} />
     </section>
   );
 }
